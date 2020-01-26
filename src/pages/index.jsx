dd91b1de4d14175003bf30729e610912d02df87c
DESKTOP-T3GUSOS\pierr <contact@pierredaily.com>
2020-01-26 20:52:36 +0800

    fix css differences between storybook and gatsby develop/build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-import "normalize.css"
-import "../scss/font.scss"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Button from "../ui/atoms/button/Button"
-import Title from "../ui/atoms/title/Title"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 import Header from "../ui/organisms/header/Header"
 
 const IndexPage = () => <Header />
