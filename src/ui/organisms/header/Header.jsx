3c180f841bf4844646946ad7a2a4a84fafc04b04
DESKTOP-T3GUSOS\pierr <contact@pierredaily.com>
2020-01-30 12:58:10 +0800

    header background image source is now adaptive

--- src/ui/organisms/header/Header.jsx
+++ src/ui/organisms/header/Header.jsx
@@ -2,13 +2,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import SignInSignUp from "../../molecules/signInSignUp/SignInSignUp"
 import Introduction from "../../molecules/introduction/Introduction"
-import Title from "../../atoms/title/Title"
 import "./Header.scss"
+
+const handleClick = () => console.log("click")
 
 const Header = () => (
   <header className="header">
     <div className="header__top">
-      <SignInSignUp />
+      <SignInSignUp
+        handleLoginClick={handleClick}
+        handleSignInClick={handleClick}
+      />
     </div>
     <div className="header__middle">
       <Introduction />
