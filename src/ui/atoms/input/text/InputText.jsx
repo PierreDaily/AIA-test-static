79ff9d033be29a4748ea0529ecb790bd086080aa
DESKTOP-T3GUSOS\pierr <contact@pierredaily.com>
2020-02-04 19:52:09 +0800

    ADD - InputText component

---
+++ src/ui/atoms/input/text/InputText.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "./InputText.scss"
+
+const InputText = ({ name, placeholder }) => (
+  <input
+    className="input__text__fluid"
+    name={name}
+    type="text"
+    placeholder={placeholder}
+  />
+)
+
+InputText.defaultProps = {
+  name: "default",
+  placeholder: "",
+}
+
+InputText.propTypes = {
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+}
+
+export default InputText
---
+++ src/ui/atoms/input/text/InputText.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import InputText from "./InputText"
+
+export default {
+  title: "Atoms/Input/Text",
+  component: InputText,
+}
+
+export const stepRed = () => <InputText placeholder="Enter your interests" />
